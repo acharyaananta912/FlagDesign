94837629e7fb5ca5b8fe0ee12a21961f02fe88bd
Bibek Acharya <acharyaananta912@gmail.com>
2024-05-17 01:01:58 -0500

    modification on plot command and fixed one of the spoke of the sun

--- Nepal/sun.py
+++ Nepal/sun.py
@@ -14,6 +14,7 @@ def make_sun(width_of_flag):
   step_size = (2*np.pi)/12
   phase_shift = step_size/2 # larger circle data points are slightly offset to the smaller circle
   sun_circle_ratio = sun_size * 1.5
+
   smaller_circle_angles = np.arange(0, 2*np.pi, step_size) + phase_shift
   larger_circle_angles  = smaller_circle_angles + phase_shift
 
@@ -24,11 +25,12 @@ def make_sun(width_of_flag):
   larger_circle_y = sun_circle_ratio * np.cos(larger_circle_angles)
 
   plt.figure(figsize=(sun_size, sun_size))
-  plt.grid("on")
+
   for i in range(len(smaller_circle_angles)):
-    plt.plot([smaller_circle_x[i], larger_circle_x[i]], [smaller_circle_y[i], larger_circle_y[i]], 'k-')
-    if i == len(smaller_circle_angles)-1:
-          plt.plot([smaller_circle_x[0], larger_circle_x[i]], [smaller_circle_y[0], larger_circle_y[i]], 'k-')
-          break
-    plt.plot([smaller_circle_x[i+1], larger_circle_x[i]], [smaller_circle_y[i+1], larger_circle_y[i]], 'k-')
-    plt.savefig("sun.png")
+        plt.plot([smaller_circle_x[i], larger_circle_x[i]], [smaller_circle_y[i], larger_circle_y[i]], 'k-')
+        plt.plot([smaller_circle_x[(i+1) % len(smaller_circle_angles)], larger_circle_x[i]], 
+                 [smaller_circle_y[(i+1) % len(smaller_circle_angles)], larger_circle_y[i]], 'k-')
+  
+  plt.gca().set_aspect('equal', adjustable='box')
+  plt.axis('off')
+  plt.savefig('fig_sun.png')
